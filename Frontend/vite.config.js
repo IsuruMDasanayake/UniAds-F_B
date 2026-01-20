26d7fdddddb3c3197def380410293a1b6166baf7
Isuru M Dasanayake <148846445+IsuruMDasanayake@users.noreply.github.com>
2026-01-21 02:04:15 +0530

    Enabled Docker Hot Reload

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Expose to Docker network
+    port: 5173,      // Internal container port
+    hmr: {
+      clientPort: 3000, // Port accessed by the browser
+    },
+    watch: {
+      usePolling: true, // Required for file sync on Windows hosts
+    },
+  },
 })
